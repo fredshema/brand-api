c7a1bd2e00fe52802d0c4525be118e0367e109e0
Fred Shema <shemafred3@gmail.com>
2024-02-22 19:49:33 +0200

    feat(deployment): Configure deployment
    - Add fly deployment configurations
    - Add github action to deploy on push to main branch
    - Refactor API with the '/api' prefix
    
    [Delivers #47]

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+import bodyParser from "body-parser";
+import express from "express";
 
-import { articleRoutes } from './routes/articles';
-import { authRoutes } from './routes/auth';
-import { userRoutes } from './routes/users';
-import './services/database';
+import { appRouter } from "./routes";
+import "./services/database";
 
 const port = 3000;
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/articles", articleRoutes);
-app.get("/uploads/:image", (req, res) => {
-  res.sendFile(req.params.image, { root: "uploads" });
-});
+app.use("/api", appRouter);
+
 
 app.listen(port, () => {
-  console.log(`started app on port ${port}`)
+  console.log(`started app on port ${port}`);
 });
