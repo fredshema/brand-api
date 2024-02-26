aa8d97082c6ec1706cb0e76560e1dd0b534dde1c
Fred Shema <shemafred3@gmail.com>
2024-02-26 18:18:48 +0200

    Update test config

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ const config: Config.InitialOptions = {
   verbose: true,
   rootDir: "./src",
   collectCoverage: true,
-  coverageReporters: ["json-summary"],
+  coverageDirectory: "../coverage",
   testTimeout: 60000,
   transform: {
     "^.+\\.tsx?$": "ts-jest",
