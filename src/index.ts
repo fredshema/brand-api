595679e69f34159808d6dd832c4bb0f08b2694d6
Fred Shema <shemafred3@gmail.com>
2024-02-29 11:18:30 +0200

    fix(cors): add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
 import appRouter from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(appRouter);
+app.use(cors());
 
 export default app;
